828caa8ce6aba99cf451a8245ac91457fd8954de
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-01-02 01:41:05 +0000

    Start

---
+++ src/main/java/com/example/adminservicemicroservice/AdminServiceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminservicemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceMicroserviceApplication.class, args);
+    }
+
+}
